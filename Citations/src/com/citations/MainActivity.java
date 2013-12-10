908d6c0ba1ab75a2c456d1e6f72675fe842d2928
luigi <luix.88@hotmail.it>
2013-12-10 23:24:28 +0100

    Added layout basics. Please pay attention to the naming conventions in the XML

--- Citations/src/com/citations/MainActivity.java
+++ Citations/src/com/citations/MainActivity.java
@@ -2,9 +2,10 @@ package com.citations;
 
 
 
-import android.os.Bundle;
 import android.app.Activity;
+import android.os.Bundle;
 import android.view.Menu;
+import android.widget.TextView;
 
 
 
@@ -16,7 +17,39 @@ public class MainActivity extends Activity
 	{
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+
+		drawLayout();
 	}
+
+
+	/**
+	 * Draw the layout for the main activity
+	 */
+	private void drawLayout()
+	{
+		String[] citation = pickRandomCitationInCategory().split("-");
+
+		TextView textView = (TextView) findViewById(R.id.activity_main_TextViewSentence);
+		textView.setText(citation[0]);
+
+		textView = (TextView) findViewById(R.id.activity_main_TextViewAuthor);
+		textView.setText(citation[1]);
+
+	}
+
+
+	/**
+	 * @return a String containing a whole (sentence + author) random citation
+	 *         within a category
+	 */
+	private String pickRandomCitationInCategory()
+	{
+		int randomNumber = 0;
+
+		return getString(R.string.inspiringAnonymous1);
+
+	}
+
 
 
 	@Override
