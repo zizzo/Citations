1758b23440b6e57a82f9811f1bc56e5d8a743a0d
Luigi <luix.88@hotmail.it>
2014-02-01 17:16:47 +0100

    Widget with no actions added

--- Citations/gen/com/citations/R.java
+++ Citations/gen/com/citations/R.java
@@ -17,76 +17,89 @@ public final class R {
          screen margins) for sw720dp devices (e.g. 10" tablets) in landscape here.
     
          */
-        public static final int activity_horizontal_margin=0x7f040000;
-        public static final int activity_vertical_margin=0x7f040001;
+        public static final int activity_horizontal_margin=0x7f050000;
+        public static final int activity_vertical_margin=0x7f050001;
+        public static final int widget_margin=0x7f050002;
     }
     public static final class drawable {
-        public static final int ic_launcher=0x7f020000;
-        public static final int share=0x7f020001;
+        public static final int app_icon=0x7f020000;
+        public static final int ic_launcher=0x7f020001;
+        public static final int preview=0x7f020002;
+        public static final int share=0x7f020003;
     }
     public static final class id {
-        public static final int action_settings=0x7f080007;
-        public static final int activity_mainImageButtonFacebook=0x7f080003;
-        public static final int activity_mainImageButtonShare=0x7f080002;
-        public static final int activity_mainImageButtonTwitter=0x7f080001;
-        public static final int activity_mainLinearLayout=0x7f080004;
-        public static final int activity_main_LinearLayoutMain=0x7f080000;
-        public static final int activity_main_TextViewAuthor=0x7f080006;
-        public static final int activity_main_TextViewSentence=0x7f080005;
+        public static final int action_settings=0x7f09000e;
+        public static final int activity_mainImageButtonFacebook=0x7f090003;
+        public static final int activity_mainImageButtonShare=0x7f090002;
+        public static final int activity_mainImageButtonTwitter=0x7f090001;
+        public static final int activity_mainLinearLayout=0x7f090004;
+        public static final int activity_main_LinearLayoutMain=0x7f090000;
+        public static final int activity_main_TextViewAuthor=0x7f090006;
+        public static final int activity_main_TextViewSentence=0x7f090005;
+        public static final int layout_appwidgetImageButtonFacebook=0x7f09000a;
+        public static final int layout_appwidgetImageButtonShare=0x7f090009;
+        public static final int layout_appwidgetImageButtonTwitter=0x7f090008;
+        public static final int layout_appwidgetLinearLayout=0x7f09000b;
+        public static final int layout_appwidget_LinearLayoutMain=0x7f090007;
+        public static final int layout_appwidget_TextViewAuthor=0x7f09000d;
+        public static final int layout_appwidget_TextViewSentence=0x7f09000c;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
+        public static final int layout_appwidget=0x7f030001;
+        public static final int layout_keyguard=0x7f030002;
     }
     public static final class menu {
-        public static final int main=0x7f070000;
+        public static final int main=0x7f080000;
     }
     public static final class string {
-        public static final int action_settings=0x7f050001;
-        public static final int app_name=0x7f050000;
-        public static final int facebookButton=0x7f050004;
-        public static final int inspiringAnonymous1=0x7f05001a;
-        public static final int inspiringAristotele1=0x7f050030;
-        public static final int inspiringBacon1=0x7f05001f;
-        public static final int inspiringCaesar1=0x7f050018;
-        public static final int inspiringCaesar2=0x7f050019;
-        public static final int inspiringCheGuevara1=0x7f050023;
-        public static final int inspiringCheGuevara2=0x7f050024;
-        public static final int inspiringChesterfield1=0x7f050020;
-        public static final int inspiringChesterton1=0x7f05001c;
-        public static final int inspiringChurchill1=0x7f050029;
-        public static final int inspiringChurchill2=0x7f05002a;
-        public static final int inspiringCoelho1=0x7f050031;
-        public static final int inspiringEinstein1=0x7f05002b;
-        public static final int inspiringEsopo1=0x7f050027;
-        public static final int inspiringFord1=0x7f050026;
-        public static final int inspiringGoethe1=0x7f05002d;
-        public static final int inspiringGoethe2=0x7f05002e;
-        public static final int inspiringJesus1=0x7f050016;
-        public static final int inspiringJohnson1=0x7f050033;
-        public static final int inspiringKennedy1=0x7f05002c;
-        public static final int inspiringKing1=0x7f050025;
-        public static final int inspiringLec1=0x7f05001d;
+        public static final int action_settings=0x7f060001;
+        public static final int app_name=0x7f060000;
+        public static final int facebookButton=0x7f060004;
+        public static final int inspiringAnonymous1=0x7f06001b;
+        public static final int inspiringAristotele1=0x7f060031;
+        public static final int inspiringBacon1=0x7f060020;
+        public static final int inspiringCaesar1=0x7f060019;
+        public static final int inspiringCaesar2=0x7f06001a;
+        public static final int inspiringCheGuevara1=0x7f060024;
+        public static final int inspiringCheGuevara2=0x7f060025;
+        public static final int inspiringChesterfield1=0x7f060021;
+        public static final int inspiringChesterton1=0x7f06001d;
+        public static final int inspiringChurchill1=0x7f06002a;
+        public static final int inspiringChurchill2=0x7f06002b;
+        public static final int inspiringCoelho1=0x7f060032;
         /**  INSPIRING 
  INSPIRING 
          */
-        public static final int inspiringLiKaShing1=0x7f050015;
-        public static final int inspiringNietzsche1=0x7f050017;
-        public static final int inspiringRoosvelt1=0x7f05002f;
-        public static final int inspiringSFrancesco1=0x7f050021;
-        public static final int inspiringSeneca1=0x7f05001b;
-        public static final int inspiringShaw1=0x7f050028;
-        public static final int inspiringTertulliano1=0x7f050022;
-        public static final int inspiringTwain1=0x7f050032;
-        public static final int inspiringUltang1=0x7f05001e;
-        public static final int lifeBacon1=0x7f05000d;
-        public static final int lifeBattaglia1=0x7f05000b;
-        public static final int lifeButturini1=0x7f05000f;
-        public static final int lifeCheGuevara1=0x7f05000c;
-        public static final int lifeChineseProverb1=0x7f050011;
-        public static final int lifeDeBono1=0x7f050013;
-        public static final int lifeDelacroix1=0x7f05000e;
-        public static final int lifeGandhi1=0x7f050009;
-        public static final int lifeGoethe1=0x7f050014;
+        public static final int inspiringDaVinci1=0x7f060015;
+        public static final int inspiringEinstein1=0x7f06002c;
+        public static final int inspiringEsopo1=0x7f060028;
+        public static final int inspiringFord1=0x7f060027;
+        public static final int inspiringGoethe1=0x7f06002e;
+        public static final int inspiringGoethe2=0x7f06002f;
+        public static final int inspiringJesus1=0x7f060017;
+        public static final int inspiringJohnson1=0x7f060034;
+        public static final int inspiringKennedy1=0x7f06002d;
+        public static final int inspiringKing1=0x7f060026;
+        public static final int inspiringLec1=0x7f06001e;
+        public static final int inspiringLiKaShing1=0x7f060016;
+        public static final int inspiringNietzsche1=0x7f060018;
+        public static final int inspiringRoosvelt1=0x7f060030;
+        public static final int inspiringSFrancesco1=0x7f060022;
+        public static final int inspiringSeneca1=0x7f06001c;
+        public static final int inspiringShaw1=0x7f060029;
+        public static final int inspiringTertulliano1=0x7f060023;
+        public static final int inspiringTwain1=0x7f060033;
+        public static final int inspiringUltang1=0x7f06001f;
+        public static final int lifeBacon1=0x7f06000d;
+        public static final int lifeBattaglia1=0x7f06000b;
+        public static final int lifeButturini1=0x7f06000f;
+        public static final int lifeCheGuevara1=0x7f06000c;
+        public static final int lifeChineseProverb1=0x7f060011;
+        public static final int lifeDeBono1=0x7f060013;
+        public static final int lifeDelacroix1=0x7f06000e;
+        public static final int lifeGandhi1=0x7f060009;
+        public static final int lifeGoethe1=0x7f060014;
         /**  This is the part dedicated App's Citations 
  Citations are divided by categories, feel free to add any citation/category or change  
  the ones that alrady exist; while doing so though, 
@@ -110,18 +123,18 @@ public final class R {
  POLITICS 
  	LIFE 
          */
-        public static final int lifeLeopardi1=0x7f050005;
-        public static final int lifeMizner1=0x7f050012;
-        public static final int lifeTiburzi1=0x7f050010;
-        public static final int lifeWest1=0x7f05000a;
-        public static final int lifeWilde1=0x7f050006;
-        public static final int lifeWilde2=0x7f050007;
-        public static final int lifeWilde3=0x7f050008;
+        public static final int lifeLeopardi1=0x7f060005;
+        public static final int lifeMizner1=0x7f060012;
+        public static final int lifeTiburzi1=0x7f060010;
+        public static final int lifeWest1=0x7f06000a;
+        public static final int lifeWilde1=0x7f060006;
+        public static final int lifeWilde2=0x7f060007;
+        public static final int lifeWilde3=0x7f060008;
         /**      CONTENT DESCRIPTION 
      CONTENT DESCRIPTION 
          */
-        public static final int sharingButton=0x7f050002;
-        public static final int twitterButton=0x7f050003;
+        public static final int sharingButton=0x7f060002;
+        public static final int twitterButton=0x7f060003;
     }
     public static final class style {
         /** 
@@ -145,10 +158,13 @@ public final class R {
     
  API 14 theme customizations can go here. 
          */
-        public static final int AppBaseTheme=0x7f060000;
+        public static final int AppBaseTheme=0x7f070000;
         /**  Application theme. 
  All customizations that are NOT specific to a particular API-level can go here. 
          */
-        public static final int AppTheme=0x7f060001;
+        public static final int AppTheme=0x7f070001;
+    }
+    public static final class xml {
+        public static final int appwidget_info=0x7f040000;
     }
 }
---
+++ Citations/src/com/citations/CitationsWidgetProvider.java
@@ -0,0 +1,19 @@
+package com.citations;
+
+import android.appwidget.AppWidgetManager;
+import android.appwidget.AppWidgetProvider;
+import android.content.Context;
+
+public class CitationsWidgetProvider extends AppWidgetProvider
+{
+	@Override
+	public void onUpdate(Context context, AppWidgetManager appWidgetManager,
+			int[] appWidgetIds)
+	{
+		super.onUpdate(context, appWidgetManager, appWidgetIds);
+
+	}
+	
+
+
+}
