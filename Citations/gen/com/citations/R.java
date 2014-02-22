40abe51d9d12ee84d9cbc6422e8bebaceafbbafb
Luigi <luix.88@hotmail.it>
2014-02-22 21:51:03 +0100

    Added true buttons to the widget. Fixed the size of the background in the widget. Trying to add pressing effect in the buttons of the widget

--- Citations/gen/com/citations/R.java
+++ Citations/gen/com/citations/R.java
@@ -43,10 +43,15 @@ public final class R {
     }
     public static final class drawable {
         public static final int app_icon=0x7f020000;
-        public static final int citations=0x7f020001;
-        public static final int ic_launcher=0x7f020002;
-        public static final int preview=0x7f020003;
-        public static final int share=0x7f020004;
+        public static final int backward_button_widget=0x7f020001;
+        public static final int citations=0x7f020002;
+        public static final int forward_button_widget=0x7f020003;
+        public static final int ic_launcher=0x7f020004;
+        public static final int logo_fb=0x7f020005;
+        public static final int logo_tw=0x7f020006;
+        public static final int preview=0x7f020007;
+        public static final int random_button_widget=0x7f020008;
+        public static final int share=0x7f020009;
     }
     public static final class id {
         public static final int action_settings=0x7f0b0019;
